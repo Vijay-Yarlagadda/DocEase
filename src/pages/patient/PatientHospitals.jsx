d24e7268c9f02a0e3c30340003f1130841bbba05
Vijay-Yarlagadda <yarlagaddavijay859@gmail.com>
2026-06-14 16:30:32 +0530

    feat: use select dropdown for appointment time to enforce 12-hour am/pm format

--- src/pages/patient/PatientHospitals.jsx
+++ src/pages/patient/PatientHospitals.jsx
@@ -255,13 +255,37 @@ const PatientHospitals = () => {
                       <Clock className="w-4 h-4 text-slate-400" />
                       Time
                     </label>
-                    <input 
-                      type="time" 
+                    <select 
                       required
                       value={time}
                       onChange={e => setTime(e.target.value)}
-                      className="dashboard-input w-full"
-                    />
+                      className="dashboard-input w-full appearance-none"
+                    >
+                      <option value="" disabled>Select a time</option>
+                      <option value="09:00 AM">09:00 AM</option>
+                      <option value="09:30 AM">09:30 AM</option>
+                      <option value="10:00 AM">10:00 AM</option>
+                      <option value="10:30 AM">10:30 AM</option>
+                      <option value="11:00 AM">11:00 AM</option>
+                      <option value="11:30 AM">11:30 AM</option>
+                      <option value="12:00 PM">12:00 PM</option>
+                      <option value="12:30 PM">12:30 PM</option>
+                      <option value="01:00 PM">01:00 PM</option>
+                      <option value="01:30 PM">01:30 PM</option>
+                      <option value="02:00 PM">02:00 PM</option>
+                      <option value="02:30 PM">02:30 PM</option>
+                      <option value="03:00 PM">03:00 PM</option>
+                      <option value="03:30 PM">03:30 PM</option>
+                      <option value="04:00 PM">04:00 PM</option>
+                      <option value="04:30 PM">04:30 PM</option>
+                      <option value="05:00 PM">05:00 PM</option>
+                      <option value="05:30 PM">05:30 PM</option>
+                      <option value="06:00 PM">06:00 PM</option>
+                      <option value="06:30 PM">06:30 PM</option>
+                      <option value="07:00 PM">07:00 PM</option>
+                      <option value="07:30 PM">07:30 PM</option>
+                      <option value="08:00 PM">08:00 PM</option>
+                    </select>
                   </div>
                   
                   <div className="pt-4">
